669cd3bfba98a4a48ec0391c3b6271b3eb5579bf
Miroslav Ondra <ondra@faster.cz>
2023-11-10 16:49:04 +0100

    Fix autoconfiguration - remove all 6-* config parts

--- run.d/50-configure-one.sh
+++ run.d/50-configure-one.sh
@@ -4,17 +4,20 @@
 
 if [ "$UNIPI_PLATFORM" = "0101" ]; then
     rm -f /etc/unipi-one.d/5*.yaml
+    rm -f /etc/unipi-one.d/6*.yaml
     ln -s /usr/share/unipi-one/5-unipi-11.yaml /etc/unipi-one.d
     rm -f /etc/unipi-one.d/0-autocal.yaml
     ln -s /usr/share/unipi-one/0-autocal.yaml /etc/unipi-one.d
 
 elif [ "$UNIPI_PLATFORM" = "1101" ]; then
     rm -f /etc/unipi-one.d/5*.yaml
+    rm -f /etc/unipi-one.d/6*.yaml
     ln -s /usr/share/unipi-one/5-unipi-lite.yaml /etc/unipi-one.d
     rm -f /etc/unipi-one.d/0-autocal.yaml
 
 elif [ "$UNIPI_PLATFORM" = "0100" ]; then
     rm -f /etc/unipi-one.d/5*.yaml
+    rm -f /etc/unipi-one.d/6*.yaml
     ln -s /usr/share/unipi-one/5-unipi-10.yaml /etc/unipi-one.d
     rm -f /etc/unipi-one.d/0-autocal.yaml
     ln -s /usr/share/unipi-one/0-autocal.yaml /etc/unipi-one.d
